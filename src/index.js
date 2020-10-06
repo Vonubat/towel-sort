17be455ace014f82dec2f95bac7f1d5f986ae4eb
Tabunov Egor <Vonubat@gmail.com>
2020-10-06 23:33:27 +0300

    feat: add comments

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
+  //  если matrix не задан, вернуть пустой массив
   if (!matrix) return [];
+  // для каждого нечетного индекса делаем реверс
   for (let i = 0; i < matrix.length; i++) {
     if (i !== 0 && i % 2 !== 0) {
       matrix[i].reverse();
     }
   }
+  // убираем вложенность
   return matrix.flat();
 }
